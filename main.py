cd9787ca8ce8c5138f7f34f6e058a63469ce8f78
Mike <mikeylofgren@gmail.com>
2025-11-15 23:52:59 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#import module
+import pygame
+
+#importing pygame.locals for easy access to key coords
+#flake8 and blk standards
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT
+)
+
+#init game
+pygame.init
+
+
